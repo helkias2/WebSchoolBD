f0ff0eae5491f1f7d36d0fe8fc30cbbacd52e427
Asif Chowdhury Aumi <rohan.keya@gmail.com>
2012-11-15 19:20:56 +0600

    js update

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@
  * 
  */
 
-$( "div.c3" ).click(function() {
-    $( this ).addClass( "big-blue", 1000, "easeOutBounce" );
+$( "li.layout-changer-2column" ).click(function() {
+    $("div#c1").toggleClass( "hide");
+    $("div#c2").toggleClass( "full");
 });
 
+$( "li.layout-changer-1column" ).click(function() {
+    $("div#right").toggleClass( "hide");
+    $("div#left").toggleClass( "full");
+});
 
-
-
+$( "li.layout-changer-displayall" ).click(function() {
+    $("div#right").removeClass( "hide");
+    $("div#left").removeClass("full");
+    $("div#left").addClass("left");
+});
 
 
 
